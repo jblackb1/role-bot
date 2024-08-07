5fb658cbe278e344c191628d73d2862c5aba691c
Jacob Blackburn <Jacob.Blackburn86@gmail.com>
2024-08-06 22:13:45 -0700

    moved bot specific functions to RoleBot object

--- bot/main.py
+++ bot/main.py
@@ -7,7 +7,6 @@ import logging
 
 from role_bot import RoleBot
 from keep_alive import keep_alive
-from discord.ext import commands
 
 # Initialize logger
 logger = logging.getLogger()
@@ -27,9 +26,6 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
-# Initialize the bot object
-bot = RoleBot(intents=intents, command_prefix="!")
-
 def load_config(file_path):
     with open(file_path, 'r') as file:
         return yaml.safe_load(file)
@@ -37,25 +33,14 @@ def load_config(file_path):
 config = load_config(config_path)
 
 API_KEY = config["API_KEY"]
+GUILD_ID = config["GUILD_ID"]
 
-async def load(bot):
-    for filename in os.listdir('./bot/cogs'):
-        if filename.endswith('cog.py'):
-            try:
-                await bot.load_extension(f'cogs.{filename[:-3]}')
-            except Exception as e:
-                logger.warning(f'{filename[:-3]} cog not started: {e}')
-
-@bot.command()
-async def sync(ctx):
-    synced = await bot.tree.sync()
-    logger.info(f"Synced {len(synced)} command(s).")
-    await ctx.send(f"Synced {len(synced)} command(s).")
+# Initialize the bot object
+bot = RoleBot(intents=intents, command_prefix="!", guild_id=GUILD_ID)
 
 async def main(bot):
     keep_alive()
     logger.info("RoleBot starting...")
-    await load(bot)
     await bot.start(API_KEY)
 
 if __name__ == "__main__":
--- bot/role_bot.py
+++ bot/role_bot.py
@@ -1,11 +1,41 @@
+import os
+import logging
+import discord
 from discord.ext import commands
 from bingo.bingo_save import GameSave
 from bingo.bingo_helper import BingoHelper
 
 
+logger = logging.getLogger(__name__)
+
 class RoleBot(commands.Bot):
-    def __init__(self, intents, command_prefix):
+    def __init__(self, intents, command_prefix, guild_id=None):
         super().__init__(intents=intents, command_prefix=command_prefix)
 
-        self.bingo_helper = BingoHelper(self)
+        self.guild_id = guild_id
         self.game_save = GameSave()
+        self.bingo_helper = BingoHelper(self)
+
+    async def setup_hook(self):
+        await self.load_cogs()
+        await self.sync_commands()
+
+    async def on_ready(self):
+        logger.info(f'Logged in as {self.user.name}')
+
+    async def load_cogs(self):
+        for filename in os.listdir('./bot/cogs'):
+            if filename.endswith('cog.py'):
+                try:
+                    await self.load_extension(f'cogs.{filename[:-3]}')
+                except Exception as e:
+                    logger.warning(f'{filename[:-3]} cog not started: {e}')
+    
+    async def sync_commands(self):
+        if self.guild_id:
+            guild = discord.Object(id=self.guild_id)
+            await self.tree.sync(guild=guild)
+            logging.info(f'Slash commands synced to guild {self.guild_id}.')
+        else:
+            await self.tree.sync()
+            logging.info('Slash commands synced globally.')
